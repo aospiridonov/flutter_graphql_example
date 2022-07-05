bda1c2c727c1122ac001ae4416ed0d75f61c6074
aospiridonov <aospiridonov@gmail.com>
2022-07-05 17:00:54 +0300

    dependency for graphql and graphql_flutter add dummy client provider

---
+++ lib/view/client_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ClientProvider extends StatelessWidget {
+  const ClientProvider({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
