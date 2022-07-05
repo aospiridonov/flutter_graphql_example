6a503db669b84a55a7d7d9a3e17fed34492efcfc
aospiridonov <aospiridonov@gmail.com>
2022-07-05 19:19:00 +0300

    add paging for review

--- lib/view/review/paging_reviews.dart
+++ lib/view/review/paging_reviews.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_graphql_example/view/review/display_reviews.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class PagingReviews extends StatelessWidget {
   const PagingReviews({Key? key}) : super(key: key);
@@ -10,8 +12,76 @@ class PagingReviews extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Paging'),
+    return Query(
+      options: QueryOptions(
+        // if we have cached results, don't clobber them
+        fetchPolicy: FetchPolicy.cacheFirst,
+        document: gql(r'''
+          query Reviews($page: Int!) {
+            reviews(page: $page) {
+              page
+              reviews {
+                id
+                episode
+                stars
+                commentary
+              }
+            }
+          }
+        '''),
+        variables: {'page': 0},
+      ),
+      builder: (
+        QueryResult result, {
+        VoidCallback? refetch,
+        FetchMore? fetchMore,
+      }) {
+        if (result.hasException) {
+          return Text(result.exception.toString());
+        }
+
+        if (result.isLoading && result.data == null) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        final nextPage = result.data?['reviews']['page'] + 1;
+
+        return Column(
+          children: <Widget>[
+            Expanded(
+              child: DisplayReviews(
+                reviews: result.data?['reviews']['reviews']
+                    .cast<Map<String, dynamic>>(),
+              ),
+            ),
+            (result.isLoading)
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ElevatedButton(
+                    onPressed: () {
+                      fetchMore!(
+                        FetchMoreOptions.partial(
+                          variables: {'page': nextPage},
+                          updateQuery: (existing, newReviews) => ({
+                            'reviews': {
+                              'page': newReviews?['reviews']['page'],
+                              'reviews': [
+                                ...existing?['reviews']['reviews'],
+                                ...newReviews?['reviews']['reviews']
+                              ],
+                            }
+                          }),
+                        ),
+                      );
+                    },
+                    child: Text('LOAD PAGE ${nextPage + 1}'),
+                  ),
+          ],
+        );
+      },
     );
   }
 }
